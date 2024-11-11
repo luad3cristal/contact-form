07f7f93c3de1210bd7355e9f02b82c18edf34adf
Luana Lima <luanalima.2006@hotmail.com>
2024-11-11 12:38:41 -0300

    feat: add and style personal-info section

---
+++ script.js
@@ -0,0 +1,9 @@
+//usar preventDefault pra evitar que a mensagem de erro apareça
+// form = document.querySelector("form");
+// input = document.querySelector("input");
+
+// form.addEventListener("submit", function (event) {
+//   if (input.value == "") {
+//     event.preventDefault(); // prevent the form from submitting
+//   }
+// });
